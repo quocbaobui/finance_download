e143ac3007ae60d77810439a6a51ce35f5e1ec58
quocbaobui <18quocbao04@gmail.com>
2025-05-13 07:58:18 +0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,13 +1,17 @@
-# config.py
+import os
 from datetime import datetime
 
 # GCS Configuration
-GCS_BUCKET_NAME = "nissan-GTR "  
+GCS_BUCKET_NAME = "nissan-GTR"  
 GCS_PROJECT_ID = "your-project-id"    
-GCS_DESTINATION_PATH = "sgx-data/"    
+GCS_DESTINATION_PATH = "sgx-data/"   
 
 # General Configuration
-LOG_FILE = r"F:\Bao_Data MiniProject_20250322\my_project\download_sgx.log"
+LOG_DIR = os.path.join(os.path.dirname(__file__), "..", "logs")
+LOG_FILE = os.path.join(LOG_DIR, "download_sgx.log")
 BASE_DATE = datetime(2025, 3, 14)  # Friday, 14 March 2025
 BASE_URL_ID = 5898
-FILE_TYPES = ["WEBPXTICK_DT.zip"]
+FILE_TYPES = ["WEBPXTICK_DT.zip"]  # Chỉ tải file WEBPXTICK_DT.zip
+
+# Create logs directory if it doesn't exist
+os.makedirs(LOG_DIR, exist_ok=True)
